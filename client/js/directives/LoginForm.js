f35b0ed8056c217b52aaa3e21fd6676189bb5a91
Kevin Denver <kevin.denver@gmail.com>
2014-01-20 10:13:35 +0000

    Refactored the login screen

--- client/js/directives/LoginForm.js
+++ client/js/directives/LoginForm.js
@@ -15,21 +15,47 @@ angular.module('myApp.directives').directive('loginForm', function() {
     },
     link: function(scope) {
 
-      scope.submitLogin = function() {
-        scope.onSubmit({user: scope.login});
-      };
+      var loginBackgroundClassName = 'loginBackground';
 
-      $('.loginBanner').mousemove(function(e) {
+      function setLoginBackground() {
+        $('body').addClass(loginBackgroundClassName);
+      }
+
+      function removeLoginBackground() {
+        $('body').removeClass(loginBackgroundClassName);
+      }
+
+      function addMouseMove() {
+        $('body').mousemove(transitionBackground);
+      }
+
+      function removeMouseMove() {
+        $('body').off('mousemove');
+      }
+
+      function transitionBackground(e) {
         var newPosition = parseInt(e.pageX/8) + "px "
           + parseInt(e.pageY/12) + "px, "
           + parseInt(e.pageX/15) + "px "
           + parseInt(e.pageY/15) + "px, "
           + parseInt(e.pageX/30) + "px "
           + parseInt(e.pageY/30) + "px";
+        $('body').css('background-position', newPosition);
+      }
 
-        $('.loginBanner').css('background-position', newPosition);
+      function cleanup() {
+        removeLoginBackground();
+        removeMouseMove();
+      }
 
-      });
+      setLoginBackground();
+      addMouseMove();
+
+      scope.submitLogin = function() {
+        scope.onSubmit({user: scope.login});
+      };
+
+      scope.$on('$destroy', cleanup);
 
     }
   }
